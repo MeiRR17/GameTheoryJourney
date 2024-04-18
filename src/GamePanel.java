eb53b178f0288a739102d055e85fa091ca94af55
MeiRR <m.b.z.dvir@gmail.com>
2024-04-19 01:39:13 +0300

    make the player move using Time

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -13,15 +13,24 @@ public class GamePanel extends JPanel implements Runnable{
     final int screenWidth = tileSize * maxScreenColumn; // 120 pixels
     final int screenHeight = tileSize * maxScreenRow;
 
+    int FPS = 30;
+
+
+    KeyControlCenter keyControlCenter = new KeyControlCenter();
     Thread gameThread;
 
+    //Player's default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel() {
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight)); // Set the wanted size of the panel
         this.setBackground(Color.black);
         this.setDoubleBuffered(true); //set this component to be double buffered
-
+        this.addKeyListener(keyControlCenter);
+        this.setFocusable(true); //make the gamePanel receive key input
 
     }
 
@@ -33,6 +42,9 @@ public class GamePanel extends JPanel implements Runnable{
     @Override
     public void run() {
 
+        double interval = (double) 1000000000 /FPS;
+        double nextDrawTime = System.nanoTime() + interval;
+
         while(gameThread != null){
 
             // UPDATE : update information
@@ -40,9 +52,37 @@ public class GamePanel extends JPanel implements Runnable{
             //DRAW : draw the screen (basically the FPS of the game)
             repaint();
 
+            try {
+                double remainingTime = nextDrawTime - System.nanoTime();
+                remainingTime/= 1000000;
+
+                if(remainingTime < 0){
+                    remainingTime = 0;
+                }
+
+                Thread.sleep((long) remainingTime);
+
+                nextDrawTime += interval;
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
         }
     }
     public void update(){
+
+        if(keyControlCenter.pressUp) {
+            playerY -= playerSpeed;
+        }
+        if(keyControlCenter.pressDown) {
+            playerY += playerSpeed;
+        }
+        if(keyControlCenter.pressRight) {
+            playerX += playerSpeed;
+        }
+        if(keyControlCenter.pressLeft) {
+            playerX -= playerSpeed;
+        }
 
     }
 
@@ -55,7 +95,7 @@ public class GamePanel extends JPanel implements Runnable{
 
         g2.setColor(Color.CYAN);
 
-        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
 
         g2.dispose(); //dispose of this graphics context and release any system resources
     }
---
+++ src/KeyControlCenter.java
@@ -0,0 +1,50 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyControlCenter implements KeyListener {
+
+    public boolean pressUp, pressDown, pressRight, pressLeft;
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        int keyCode = e.getKeyCode();
+
+        if(keyCode == KeyEvent.VK_UP) {
+            pressUp = true;
+        }
+        if(keyCode == KeyEvent.VK_DOWN) {
+            pressDown = true;
+        }
+        if(keyCode == KeyEvent.VK_RIGHT) {
+            pressRight = true;
+        }
+        if(keyCode == KeyEvent.VK_LEFT) {
+            pressLeft = true;
+        }
+
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+
+        if(keyCode == KeyEvent.VK_UP) {
+            pressUp = false;
+        }
+        if(keyCode == KeyEvent.VK_DOWN) {
+            pressDown = false;
+        }
+        if(keyCode == KeyEvent.VK_RIGHT) {
+            pressRight = false;
+        }
+        if(keyCode == KeyEvent.VK_LEFT) {
+            pressLeft = false;
+        }
+    }
+}
